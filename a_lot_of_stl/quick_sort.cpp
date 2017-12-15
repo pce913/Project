9723721f6b9b1713d3a7c684a132f9d8cf5c0b38
pce913 <pce00913@naver.com>
2017-12-16 07:48:52 +0900

    Update quick_sort.cpp

--- a_lot_of_stl/quick_sort.cpp
+++ a_lot_of_stl/quick_sort.cpp
@@ -1,3 +1,4 @@
+#include<stdio.h>
 //2751번: 수 정렬하기2 문제로 검수 완료.
 template <typename T>
 void swap(T& x, T& y){
@@ -10,16 +11,12 @@ template <typename T>
 void sort(T arr[],int start,int end) { //vector를 정렬시키고 싶을땐 T를 T&로 바꿔줘야 한다.
 	if (start >= end)
 		return;
-	int left = start;
-	int right = end;
+	int left = start - 1;
+	int right = end + 1;
 	T pivot = arr[(start + end) / 2];
 	while (true) {
-		while (arr[left] < pivot){
-			left++;
-		}
-		while (arr[right] > pivot){
-			right--;
-		}
+		while (arr[++left] < pivot);
+		while (arr[--right] > pivot);    //operator < 를 사용하기 위해선 사실 arr[right] > pivot 이게 더 낫다.
 		if (left >= right)
 			break;
 		swap(arr[left], arr[right]);
